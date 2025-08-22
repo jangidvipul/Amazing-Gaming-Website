90323c7d68b2a659c3f57fd3b38da8d02da83606
vipul <vipuljangid141@gmail.com>
2025-08-22 12:37:59 +0530

    completed

---
+++ main.js
@@ -0,0 +1,23 @@
+const nextbtn = document.querySelector('.next-btn');
+const video = document.querySelector('.hero-video');
+
+
+const movieList = ['videos/hero-1.mp4', 'videos/hero-2.mp4', 'videos/hero-3.mp4', 'videos/hero-4.mp4'];
+
+let index = 0;
+nextbtn.addEventListener("click",()=>{
+   index++;
+   video.src = movieList[index];
+
+   if(index === 3){
+      index = -1;
+   }
+   
+});
+
+
+
+
+
+
+
